654fa80b04d26a2f723e2d75265bea824234c8d7
abuden98 <fikryizuddin1998@gmail.com>
2021-02-07 00:43:28 +0800

    Initial commit

---
+++ app/src/main/java/com/mmu/foodbank/Child.java
@@ -0,0 +1,67 @@
+package com.mmu.foodbank;
+
+public class Child {
+
+    private String userName;
+    private String foodName;
+    private String quantity;
+    private String number;
+    private String imageUri;
+    private Double x,y;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+
+    public Double getX() {
+        return x;
+    }
+
+    public void setX(Double x) {
+        this.x = x;
+    }
+
+    public Double getY() {
+        return y;
+    }
+
+    public void setY(Double y) {
+        this.y = y;
+    }
+}
